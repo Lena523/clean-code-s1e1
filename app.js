5062d18fd0365e89f67caf93178f020888ceff4f
Elena Khadasevich <camatsu90@mail.ru>
2024-12-13 00:57:53 +0300

    refactor(basic-3.3):  add concise names to classes and identifiers

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -18,7 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-    listItem.setAttribute('class','li');
+    listItem.setAttribute('class','task-item');
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
@@ -37,7 +37,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
-    checkBox.className="input-checkbox";
+    checkBox.className="task-checkbox";
     editInput.type="text";
     editInput.className="task input";
 
@@ -155,7 +155,7 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector(".input-checkbox[type=checkbox]");
+    var checkBox=taskListItem.querySelector(".task-checkbox[type=checkbox]");
     var editButton=taskListItem.querySelector(".button.edit");
     var deleteButton=taskListItem.querySelector(".button.delete");
 
